abaef8f91995f8c0a973b84df91f79a5cae4b9c1
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-01-14 23:33:33 +0530

    my first program

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+else if(n<0)
+{
+printf("Negative number");
+}
+else
+{
+printf("Zero");
+}
+}
