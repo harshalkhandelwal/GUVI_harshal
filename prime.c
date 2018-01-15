02a5e08a0850e2b5f2116d16ee5c43e0dcc389e6
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-01-16 01:29:36 +0530

    to check prime number or not:

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,p=0;
+    printf("enter the number: ");
+    scanf("%d",&n);
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            p=1;
+            break;
+        }
+    }
+    if(p==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
