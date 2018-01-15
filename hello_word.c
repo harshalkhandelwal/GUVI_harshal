6c8febd70c8c7eb8c56cd44fe576a2d5c8fed51e
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-01-15 23:14:19 +0530

    to print hello word N times

---
+++ hello_word.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the number of times you want to print hello: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Hello\n");
+    }
+}
