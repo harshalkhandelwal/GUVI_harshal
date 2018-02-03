c9722ae1a31464070361718de44e3eeaf3c254e2
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-02-03 19:20:21 +0530

    to check the number in range

---
+++ number_in_range.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the number: ");
+    scanf("%d",&n);
+    if(n>=1 && n<=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
