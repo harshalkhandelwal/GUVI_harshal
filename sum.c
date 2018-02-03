016c5aa48a56af29644460d2d7dc991e967eaa0c
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-02-03 19:55:36 +0530

    to print the sum of 2 numbers

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the first number: ");
+    scanf("%d",&a);
+    printf("enter the second number: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d",c);
+}
