9fe6e182f6ec6ac808d9959528c52a15218b1de2
harshalkhandelwal <35273066+harshalkhandelwal@users.noreply.github.com>
2018-04-15 18:38:28 +0530

    to print the minimum among the given numbers

---
+++ print_minimum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[100];
+    printf("enter the number of elements: ");
+    scanf("%d",&n);
+    printf("enter the elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[0]>a[i])
+        {
+            a[0]=a[i];
+        }
+    }
+    printf("%d",a[0]);
+}
+
